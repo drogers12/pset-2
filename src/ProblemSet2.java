20de4deebaa2d4fad8261f6dfef6f59ce4ac8caa
David Rogers <drogers@ucvts.tec.nj.us>
2019-10-01 11:36:38 -0400

    Exercise 5 commit

--- src/ProblemSet2.java
+++ src/ProblemSet2.java
@@ -28,27 +28,21 @@ public class ProblemSet2 {
          * name, grade, age, and hometown.
          */
 
-         // System.out.print("\nEnter your first name: ");
-         // String firstName = in.nextLine();
-         //
-         // System.out.print("Enter your last name: ");
-         // String lastName = in.nextLine();
-         //
-         // System.out.print("Enter your grade: ");
-         // int gradeLevel = in.nextInt();
-         //
-         // System.out.print("Enter your age: ");
-         // int age = in.nextInt();
-         // in.nextLine();
-         //
-         // System.out.print("Enter your hometown: ");
-         // String homeTown = in.nextLine();
-         //
-         //
-         // System.out.println("\nNAME \t :" + firstName + " " + lastName);
-         // System.out.println("GRADE \t :" + gradeLevel);
-         // System.out.println("AGE \t :" + age);
-         // System.out.println("HOMETOWN :" + homeTown);
+        System.out.print("\nEnter your first name: ");
+        String firstName = in.nextLine();
+        System.out.print("Enter your last name: ");
+        String lastName = in.nextLine();
+        System.out.print("Enter your grade: ");
+        String grade = in.nextLine();
+        System.out.print("Enter your age: ");
+        String age = in.nextLine();
+        System.out.print("Enter your hometown: ");
+        String hometown = in.nextLine();
+
+        System.out.printf("\nNAME     : %s %s\n", firstName, lastName);
+        System.out.printf("GRADE    : %s\n", grade);
+        System.out.printf("AGE      : %s\n", age);
+        System.out.printf("HOMETOWN : %s\n", hometown);
 
         /*
          * Exercise 2.
@@ -57,45 +51,29 @@ public class ProblemSet2 {
          * bills, quarters, dimes, nickels, and pennies needed to produce this amount.
          */
 
-         System.out.print("Enter a dollar amount: ");
-         double moneyTotal = in.nextDouble();
+        final double VALUE_DOLLAR = 1.00;
+        final double VALUE_QUARTER = 0.25;
+        final double VALUE_DIME = 0.10;
+        final double VALUE_NICKEL = 0.05;
 
-         final double dollarValue = 1;
-         final double quarterValue = .25;
-         final double dimeValue = .1;
-         final double nickelValue = .05;
+        System.out.print("\nEnter a dollar amount: ");
+        double value = in.nextDouble();
 
+        double dollarsNeeded = Math.floor(value/VALUE_DOLLAR);
+        value -= dollarsNeeded*VALUE_DOLLAR;
+        double quartersNeeded = Math.floor(value/VALUE_QUARTER);
+        value -= quartersNeeded*VALUE_QUARTER;
+        double dimesNeeded = Math.floor(value/VALUE_DIME);
+        value -= dimesNeeded*VALUE_DIME;
+        double nickelsNeeded = Math.floor(value/VALUE_NICKEL);
+        value -= nickelsNeeded*VALUE_NICKEL;
+        double penniesNeeded = Math.floor(value/0.01);
 
-
-
-         double moneyLeft = moneyTotal % dollarValue;
-         double dollarAmount = moneyTotal-(moneyLeft);
-         int integerDollarAmount = (int) Math.round (dollarAmount);
-         System.out.println("DOLLARS\t : " + integerDollarAmount);
-
-
-         double quarterAmount = (moneyLeft-(moneyLeft % quarterValue))/quarterValue;
-         moneyLeft = moneyTotal-dollarAmount-(quarterAmount*quarterValue);
-         int integerQuarterAmount = (int) Math.round (quarterAmount);
-         System.out.println("QUARTERS : " + integerQuarterAmount);
-
-
-         double dimeAmount = (moneyLeft-(moneyLeft % dimeValue))/dimeValue;
-         moneyLeft = moneyTotal-dollarAmount-(quarterAmount*quarterValue)-(dimeAmount*dimeValue);
-         int integerDimeAmount = (int) Math.round (dimeAmount);
-         System.out.println("DIMES\t : " + integerDimeAmount);
-
-
-         double nickelAmount = (moneyLeft-(moneyLeft % nickelValue))/nickelValue;
-         moneyLeft = moneyTotal-dollarAmount-(quarterAmount*quarterValue)-(dimeAmount*dimeValue)-(nickelAmount*nickelValue);
-         int integerNickelAmount = (int) Math.round (nickelAmount);
-         System.out.println("NICKELS\t : " + integerNickelAmount);
-
-         double pennieAmount = moneyLeft*100;
-         int integerPennieAmount = (int) Math.round (pennieAmount);
-         System.out.println("PENNIES\t : " + integerPennieAmount);
-
-
+        System.out.printf("\nDOLLARS  : %.0f\n", dollarsNeeded);
+        System.out.printf("QUARTERS : %.0f\n", quartersNeeded);
+        System.out.printf("DIMES    : %.0f\n", dimesNeeded);
+        System.out.printf("NICKELS  : %.0f\n", nickelsNeeded);
+        System.out.printf("PENNIES  : %.0f\n\n", penniesNeeded);
 
         /*
          * Exercise 3.
@@ -104,49 +82,34 @@ public class ProblemSet2 {
          * bills and coins needed to produce this amount.
          */
 
-         System.out.print("Enter a dollar amount: ");
-         moneyTotal = in.nextDouble();
+        final double VALUE_TWENTY_DOLLAR = 20.00;
+        final double VALUE_TEN_DOLLAR = 10.00;
+        final double VALUE_FIVE_DOLLAR = 5.00;
 
-         final double tenDollarBillValue = 10;
-         final double fiveDollarBillValue = 5;
+        System.out.print("Enter a dollar amount: ");
+        value = in.nextDouble();
 
-         double tenDollarBillAmount = (moneyTotal-(moneyTotal%tenDollarBillValue))/tenDollarBillValue;
-         int integerTenDollarBillAmount = (int) Math.round (tenDollarBillAmount);
+        double twentiesNeeded = Math.floor(value/VALUE_TWENTY_DOLLAR);
+        value -= twentiesNeeded*VALUE_TWENTY_DOLLAR;
+        double tensNeeded = Math.floor(value/VALUE_TEN_DOLLAR);
+        value -= tensNeeded*VALUE_TEN_DOLLAR;
+        double fivesNeeded = Math.floor(value/VALUE_FIVE_DOLLAR);
+        value -= fivesNeeded*VALUE_FIVE_DOLLAR;
+        dollarsNeeded = Math.floor(value/VALUE_DOLLAR);
+        value -= dollarsNeeded*VALUE_DOLLAR;
+        quartersNeeded = Math.floor(value/VALUE_QUARTER);
+        value -= quartersNeeded*VALUE_QUARTER;
+        dimesNeeded = Math.floor(value/VALUE_DIME);
+        value -= dimesNeeded*VALUE_DIME;
+        nickelsNeeded = Math.floor(value/VALUE_NICKEL);
+        value -= nickelsNeeded*VALUE_NICKEL;
+        penniesNeeded = Math.floor(value/0.01);
 
-         double fiveDollarBillAmount = ((moneyTotal-(tenDollarBillAmount*tenDollarBillValue)%fiveDollarBillValue))/fiveDollarBillValue;
-         int integerFiveDollarBillAmount = (int) Math.round (fiveDollarBillAmount);
+        double billsNeeded = twentiesNeeded+tensNeeded+fivesNeeded+dollarsNeeded;
+        double coinsNeeded = quartersNeeded+dimesNeeded+nickelsNeeded+penniesNeeded;
 
-
-
-
-
-         moneyLeft = moneyTotal % dollarValue;
-         dollarAmount = moneyTotal-(moneyLeft);
-         integerDollarAmount = (int) Math.round (dollarAmount);
-
-         int billsTotal = integerTenDollarBillAmount + integerFiveDollarBillAmount;
-         System.out.println(billsTotal);
-
-         quarterAmount = (moneyLeft-(moneyLeft % quarterValue))/quarterValue;
-         moneyLeft = moneyTotal-dollarAmount-(quarterAmount*quarterValue);
-         integerQuarterAmount = (int) Math.round (quarterAmount);
-
-
-         dimeAmount = (moneyLeft-(moneyLeft % dimeValue))/dimeValue;
-         moneyLeft = moneyTotal-dollarAmount-(quarterAmount*quarterValue)-(dimeAmount*dimeValue);
-         integerDimeAmount = (int) Math.round (dimeAmount);
-
-
-         nickelAmount = (moneyLeft-(moneyLeft % nickelValue))/nickelValue;
-         moneyLeft = moneyTotal-dollarAmount-(quarterAmount*quarterValue)-(dimeAmount*dimeValue)-(nickelAmount*nickelValue);
-         integerNickelAmount = (int) Math.round (nickelAmount);
-
-         pennieAmount = moneyLeft*100;
-         integerPennieAmount = (int) Math.round (pennieAmount);
-
-         int coinsTotal = integerQuarterAmount+integerDimeAmount+integerNickelAmount+integerPennieAmount;
-         System.out.println(coinsTotal);
-
+        System.out.printf("\nBILLS : %.0f\n", billsNeeded);
+        System.out.printf("COINS : %.0f\n\n", coinsNeeded);
 
         /*
          * Exercise 4.
@@ -155,7 +118,24 @@ public class ProblemSet2 {
          * and inches.
          */
 
+        final double MILES_TO_INCHES = 63360;
+        final double YARDS_TO_INCHES = 36;
+        final double FEET_TO_INCHES = 12;
 
+        System.out.print("Enter a number of inches: ");
+        double inches = in.nextDouble();
+
+        double miles = Math.floor(inches/MILES_TO_INCHES);
+        inches -= miles*MILES_TO_INCHES;
+        double yards = Math.floor(inches/YARDS_TO_INCHES);
+        inches -= yards*YARDS_TO_INCHES;
+        double feet = Math.floor(inches/FEET_TO_INCHES);
+        inches -= feet*FEET_TO_INCHES;
+
+        System.out.printf("\nMILES  : %.0f\n", miles);
+        System.out.printf("YARDS  : %.0f\n", yards);
+        System.out.printf("FEET   : %.0f\n", feet);
+        System.out.printf("INCHES : %.0f\n\n", inches);
 
         /*
          * Exercise 5.
@@ -164,7 +144,20 @@ public class ProblemSet2 {
          * meters, and centimeters.
          */
 
+        final double KILOMETERS_TO_CENTIMETERS = 100000;
+        final double METERS_TO_CENTIMETERS = 100;
 
+        System.out.print("Enter a number of centimeters: ");
+        double centimeters = in.nextDouble();
+
+        double kilometers = Math.floor(centimeters/KILOMETERS_TO_CENTIMETERS);
+        centimeters -= kilometers*KILOMETERS_TO_CENTIMETERS;
+        double meters = Math.floor(centimeters/METERS_TO_CENTIMETERS);
+        centimeters -= meters*METERS_TO_CENTIMETERS;
+
+        System.out.printf("\nKILOMETERS  : %.0f\n", kilometers);
+        System.out.printf("METERS      : %.0f\n", meters);
+        System.out.printf("CENTIMETERS : %.0f\n\n", centimeters);
 
         /*
          * Exercise 6.
@@ -172,7 +165,7 @@ public class ProblemSet2 {
          * Given a diameter, print the area and circumference of the corresponding circle.
          */
 
-
+        
 
         /*
          * Exercise 7.
